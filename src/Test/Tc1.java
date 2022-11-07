8547bab9b18c505a51f01e858bee88c4379527c1
tahmina168 <tkhanom66@gmail.com>
2022-11-06 21:08:43 -0600

    java2

---
+++ src/Test/Tc1.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Tc1 {
+
+	public static void main(String[] args) {
+		System.out.println("TC1");
+
+	}
+
+}
---
+++ src/Test/Tc2.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Tc2 {
+
+	public static void main(String[] args) {
+		System.out.println("Tc2");
+
+	}
+
+}
